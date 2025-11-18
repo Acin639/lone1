74af65236bd79d38c06105ab318cafaf2de08fb3
Acin639 <titusnjeru48@gmail.com>
2025-11-18 16:27:47 +0300

    Hi

--- src/components/searcher/Results.jsx
+++ src/components/searcher/Results.jsx
@@ -1,15 +1,17 @@
 
 import React from 'react';
+import search_icon from '../../assets/search_icon.svg'
 
 export default function Results({movies}) {
   return (
     <div className="results-box">
       {movies.map(movie => (
         <div key={movie.id} className="result-item">
-          <img src="/icons/search.svg" className="result-icon" alt="" />
-          {movie.title}
+          <img src={search_icon} className="result-icon" alt="" />
+          {movie.title + (movie.release_date ? ` (${movie.release_date.split('-')[0]})` : '')} 
         </div>
       ))}
     </div>
   );
 }
+
